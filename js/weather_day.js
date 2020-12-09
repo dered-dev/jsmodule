278ffa06eb3b5f0bb4e55593f9b6fb5880eb0dd7
jorge luis camarillo cristobal <jlcc170@gmail.com>
2020-12-08 21:40:05 -0600

    UPDATE weather day with switch sentence

--- js/weather_day.js
+++ js/weather_day.js
@@ -1,18 +1,35 @@
+
+
+// if (WEATHER === 'soleado') {
+//     console.log('oh!, que buen clima para estar en la playa')
+// } else if (WEATHER === 'lluvioso') {
+//     console.log('oh!, que buen clima para dormir')
+// } else if (WEATHER === 'nevando') {
+//     console.log('oh!, que buen clima para quedarse en casa')
+// } else if (WEATHER === 'nublado') {
+//     console.log('oh!, que buen clima para tomar café')
+// } else {
+//     const WEATHER_DESCRIPTION = prompt('¿Describime tu día?')
+//     console.log(WEATHER_DESCRIPTION)
+// }
 const WEATHER = prompt('¿Cómo está el clima?')
 
-if (WEATHER === 'soleado') {
-    console.log('oh!, que buen clima para estar en la playa')
-} else if (WEATHER === 'lluvioso') {
-    console.log('oh!, que buen clima para dormir')
-} else if (WEATHER === 'nevando') {
-    console.log('oh!, que buen clima para quedarse en casa')
-} else if (WEATHER === 'nublado') {
-    console.log('oh!, que buen clima para tomar café')
-} else {
-    const WEATHER_DESCRIPTION = prompt('¿Describime tu día?')
-    console.log(WEATHER_DESCRIPTION)
+switch(WEATHER) {
+    case 'soleado': 
+        console.log('oh!, que buen clima para estar en la playa')
+    break
+    case 'lluvioso': 
+        console.log('oh!, que buen clima para estar en la playa')
+    break
+    case 'nevando': 
+        console.log('oh!, que buen clima para quedarse en casa')
+    break
+    case 'nublado': 
+        console.log('oh!, que buen clima para tomar café')
+    break
+    default:
+        const WEATHER_DESCRIPTION = prompt('¿Describime tu día?')
+        console.log(WEATHER_DESCRIPTION)
 }
-
-
 
 
