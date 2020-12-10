b323b2c15fce6aca8fd86f46dc239c5415db16de
jorge luis camarillo cristobal <jlcc170@gmail.com>
2020-12-09 22:43:48 -0600

    UPDATE master branch to default

--- js/main.js
+++ js/main.js
@@ -1,29 +1,5 @@
 console.log('Hola Koders')
 
-// variables var , Let , Const
-// let si va a cambiar mi valor
-// const si es un valor fijo
-// snake_case
-// Const
-const NAME_KODER = 'oscar'
-// let
-// Case Styles
-// camelCase
-// PascalCase
-// snake_case
-
-let name_koder = prompt('¿Cuál es tu nombre?')
-console.log(name_koder)
-
-let age_koder = prompt('¿Cuál es tu edad?')
-
-let resultSuma = parseFloat(age_koder) + 5
-console.log('suma: ', resultSuma )
-console.log('multi: ', age_koder * 5 )
-console.log('div: ', age_koder / 5 )
-console.log('rest: ', age_koder - 5 )
-
-
 
 
 
