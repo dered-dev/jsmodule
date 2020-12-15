fd7d60468f6e83aa1f568913104ef2eb2108dcd9
jorge luis camarillo cristobal <jlcc170@gmail.com>
2020-12-14 20:39:17 -0600

    ADD intro arrow functions

---
+++ js/arrow_functions.js
@@ -0,0 +1,16 @@
+
+function add(a, b){
+   // sentencias 
+   return a + b
+}
+const result = add(2, 3)
+console.log(result)
+
+
+const sumar = (a, b) => {
+    return a + b 
+}
+const result = sumar(2, 3)
+console.log(result)
+
+
