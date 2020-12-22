8b1687fb3b6c9a3add88f86a6106dbe0bd71c145
jorge luis camarillo cristobal <jlcc170@gmail.com>
2020-12-21 22:57:23 -0600

    ADD events and homework

---
+++ js/homework.js
@@ -0,0 +1,9 @@
+/**
+ * onchange() lo tiene que disparar el selector
+ * get value option selected 
+ * document.getElementById('orden').value
+ * 1 función
+ * 1 metodo
+ * usar innerHTML
+ * https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/change_event
+ */
