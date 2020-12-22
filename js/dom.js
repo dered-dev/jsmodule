4ff6653c25822d18094bef63ff30491eee13b8ab
jorge luis camarillo cristobal <jlcc170@gmail.com>
2020-12-21 23:13:17 -0600

    ADD selectors by class, tag, id and queryselector

--- js/dom.js
+++ js/dom.js
@@ -12,5 +12,23 @@ console.log(parrafo)
 document.getElementById('caja2').appendChild(parrafo)
 
 
+// selectores JS
 
+
+// let elemento = document.getElementById('elementToSet')
+let elementos = document.getElementsByClassName('parrafos')
+let secondElement = elementos[1]
+// secondElement.setAttribute('class','parrafos parrafo2 parrafoprueba active')
+secondElement.classList.add('active')
+secondElement.classList.remove('parrafoprueba')
+let bytag = document.getElementsByTagName('section')
+console.log(bytag)
+bytag[0].classList.add('section__parrafo')
+
+let secciones = document.getElementsByClassName('secciones')
+// let secciones = document.getElementsByClassName('sinclase')
+// let querySelector = document.querySelector('.secciones')
+// 1er coincidencia
+let querySelectorAll = document.querySelectorAll('li')
+console.log(querySelectorAll)
 
