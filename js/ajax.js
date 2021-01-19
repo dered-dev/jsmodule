917fdcea8da275c84a7ebbf50842b39dbcc1d773
jorge luis camarillo cristobal <jlcc170@gmail.com>
2021-01-18 22:18:58 -0600

    ADD modal to delete

--- js/ajax.js
+++ js/ajax.js
@@ -9,13 +9,13 @@ $(function(){
         for(item in users) {
             usersHtml += `
                 <tr>
-                    <td>${users[item].name}</td>
+                    <td class="tdname">${users[item].name}</td>
                     <td>${users[item].lastname}</td>
                     <td><a href="${users[item].urlPhoto}">Photo</a></td>
                     <td>
                         <a href="show-user.html?id=${item}">Ver</a>
                         <a href="update-user.html?id=${item}">Actualizar</a>
-                        <a data-id="${item}">Eliminar</a>
+                        <a data-id="${item}" class="delete__user" data-bs-toggle="modal" data-bs-target="#mymodal">Eliminar</a>
                     </td>
                 </tr>
             `
@@ -30,7 +30,12 @@ $(function(){
             method: 'GET'
         }).done(function(response) {
             paintUsers(response)
-            
+            //console.log(response)
+            // let arr = []
+            // for(item in response) {
+            //     arr.push(response[item])
+            // }
+            // console.log(arr.reverse())
         }).fail(function(err){
             console.log(err)
             console.log(err.status)
@@ -64,7 +69,32 @@ $(function(){
         })
     }
 
-    
+    $('#delete__user__ok').click(function(){
+        console.log('eliminando')
+        let iduser = $(this).data('id')
+        $.ajax({
+            url: `https://koders1gpython-default-rtdb.firebaseio.com/jorge/users/${iduser}.json`,
+            method: 'DELETE'
+        }).done(function(response) {
+            console.log('usuario eliminado')
+            location.pathname = '/ajax.html'
+        }).fail(function(err){
+            console.log(err)
+            console.log(err.status)
+            console.log(err.statusText)
+            console.log('todo mal')
+        })
+    })
+
+    $('.users').on('click','.delete__user', function(){
+        let iduser = $(this).data('id')
+        $('#mymodal #delete__user__ok').attr('data-id', iduser)
+    })
+
+    $('.users').on('click','.tdname', function(){
+        console.log('click en el nombre')
+    })
+
 
     // listado
     // new user
@@ -73,7 +103,7 @@ $(function(){
     // update user
     // UPDATE
 
-    // delete User
+    // delete User *
     // DELETE
 
 
